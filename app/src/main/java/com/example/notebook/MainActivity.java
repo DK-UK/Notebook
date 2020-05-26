685a79e44a1b6aef4fd045e4a79df3295a4cbe83
DK <dk563177@gmail.com>
2020-05-26 08:46:56 +0530

    changed color

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -12,11 +14,14 @@ import androidx.fragment.app.FragmentTransaction;
 public class MainActivity extends AppCompatActivity implements bottomSheet.onItemClickDeleteListener{
 public static final String TAG = "Main";
 Activity activity;
+ActionBar toolbar;
 static FragmentManager fm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = getSupportActionBar();
+        toolbar.setBackgroundDrawable(getResources().getDrawable(R.drawable.toolbar_settings));
         activity = this;
         recyclerviewFragment recyclerviewFragment = new recyclerviewFragment();
         replaceFragment(recyclerviewFragment);
